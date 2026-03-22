e59332f5a6797bcd15f2bf869d50ebbc949fb222
Briek <briek.rombaut@ugent.be>
2026-03-22 17:14:42 +0100

    bumped version

---
+++ lora_p2p/__main__.py
@@ -0,0 +1,7 @@
+from .reliable_communicating_node import testReliableCommunicatingNode
+
+# Run this file using `python -m lora_p2p`
+
+if __name__ == "__main__":
+    # INTEGRATION TEST
+    testReliableCommunicatingNode()
