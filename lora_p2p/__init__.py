ef191489fdcd489023e4e66c0a1630ce4bf174cf
Briek <briek.rombaut@ugent.be>
2026-03-26 15:52:07 +0100

    exposed CommunicationParameters from the python module

--- lora_p2p/__init__.py
+++ lora_p2p/__init__.py
@@ -1,7 +1,9 @@
 from .lora_node import LoRaNode
 from .reliable_communicating_node import ReliableCommunicatingNode
+from .lora_kit.lora_kit_controller import CommunicationParameters
 
 __all__ = [
     "LoRaNode",
     "ReliableCommunicatingNode",
+    "CommunicationParameters"
 ]
