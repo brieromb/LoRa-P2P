01c70fc1d8b3125dcd945d3f2a634d605c4963b3
Briek <briek.rombaut@ugent.be>
2026-03-27 11:34:14 +0100

    exposed ConnectionQualityMeasurements in the module interface

--- lora_p2p/__init__.py
+++ lora_p2p/__init__.py
@@ -1,9 +1,11 @@
 from .lora_node import LoRaNode
 from .reliable_communicating_node import ReliableCommunicatingNode
 from .lora_kit.lora_kit_controller import CommunicationParameters
+from .receiving.received_message import ConnectionQualityMeasurements
 
 __all__ = [
     "LoRaNode",
     "ReliableCommunicatingNode",
-    "CommunicationParameters"
+    "CommunicationParameters",
+    "ConnectionQualityMeasurements"
 ]
